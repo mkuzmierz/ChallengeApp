4d6f764a425921883756c422e8d4c268adae3682
Gosia Kuzmierz <m.kuzmierz@gmail.com>
2024-03-01 10:28:36 +0100

    ifs_exercise

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Gosia";
+var gender = "female";
+var age = 34;
+
+if (gender == "female" && age < 30)
+{
+    Console.WriteLine("Woman below 30 years old");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, 30 years old");
+}
+else if (gender == "male" && age < 18)
+{
+    Console.WriteLine("Underage man");
+}
